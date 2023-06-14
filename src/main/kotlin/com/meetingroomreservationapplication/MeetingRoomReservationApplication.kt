5af0c2107b5f3de3583a1c48235c67ee212e649c
Richard Bílek <richard.bilek@volny.cz>
2023-06-14 13:35:41 +0200

    added port configuration

--- src/main/kotlin/com/meetingroomreservationapplication/MeetingRoomReservationApplication.kt
+++ src/main/kotlin/com/meetingroomreservationapplication/MeetingRoomReservationApplication.kt
@@ -1,11 +1,22 @@
 package com.meetingroomreservationapplication
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationListener
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
-open class MeetingRoomReservationApplication
+class MeetingRoomReservationApplication
 
 fun main(args: Array<String>) {
     runApplication<MeetingRoomReservationApplication>(*args)
+}
+
+@Component
+class ApplicationStartup : ApplicationListener<ApplicationReadyEvent> {
+    override fun onApplicationEvent(event: ApplicationReadyEvent) {
+        val port = event.applicationContext.environment.getProperty("server.port")
+        println("Server is running on port: $port")
+    }
 }
